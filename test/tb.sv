5af776ba790f5a6554fa4f85e19a1ba5ef4bfac8
Niels Hartmann <nwhartmann@gmail.com>
2026-03-09 20:55:39 +0100

    test: add tb wrapper with DUT instantiation and enable tracing
    - Add tt_um_simple_dcache instantiation to tb.sv
    - Add trace options to Makefile for waveform generation
    - Switch to use tt_um_dcache simple test module

--- test/tb.sv
+++ test/tb.sv
@@ -1,12 +1,23 @@
 module tt_um_simple_dcache_tb (
-    input logic [7:0] ui_in,    // Dedicated inputs
-    input logic [7:0] uo_out,   // Dedicated outputs
-    input logic [7:0] uio_in,   // IOs: Input path
-    input logic [7:0] uio_out,  // IOs: Output path
-    input logic [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input logic       ena,      // always 1 when the design is powered, so you can ignore it
-    input logic       clk,      // clock
-    input logic       rst_n     // reset_n - low to reset
+    input  logic [7:0] ui_in,    // Dedicated inputs
+    output logic [7:0] uo_out,   // Dedicated outputs
+    input  logic [7:0] uio_in,   // IOs: Input path
+    inout  logic [7:0] uio_out,  // IOs: Output path
+    output logic [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  logic       ena,      // always 1 when the design is powered, so you can ignore it
+    input  logic       clk,      // clock
+    input  logic       rst_n     // reset_n - low to reset
 );
+
+  tt_um_simple_dcache dut (
+      .ui_in   (ui_in),
+      .uo_out  (uo_out),
+      .uio_in  (uio_in),
+      .uio_out (uio_out),
+      .uio_oe  (uio_oe),
+      .ena     (ena),
+      .clk     (clk),
+      .rst_n   (rst_n)
+  );
 
 endmodule
